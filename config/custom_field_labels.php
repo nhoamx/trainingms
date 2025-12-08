e598d61de86661a2a57ad57b7ceb11f0e1ccc1af
Alfredo Galvan <chencho.backup@gmail.com>
2025-12-08 04:46:07 -0600

    feat: comment visibility

---
+++ config/custom_field_labels.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Field Labels
+    |--------------------------------------------------------------------------
+    |
+    | Este archivo contiene los labels correctos para los campos personalizados
+    | de evaluaciones. Si un key_label está mal escrito en la base de datos,
+    | se mostrará el label correcto desde aquí.
+    |
+    */
+
+    'codigolinea' => 'Código de Línea',
+    'gerente' => 'Gerente',
+    'lider_de_linea' => 'Líder de Línea',
+    'numero' => 'Número',
+    'superintendente' => 'Superintendente',
+    'supervisor' => 'Supervisor',
+];
