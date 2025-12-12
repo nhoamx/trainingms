aecd834f8ca08089a331c3df8308e7205ffb90aa
Alfredo Galvan <chencho.backup@gmail.com>
2025-12-12 07:18:53 -0600

    feat: hide name

--- resources/js/Pages/Reports/LikertOrganizationReport.vue
+++ resources/js/Pages/Reports/LikertOrganizationReport.vue
@@ -215,7 +215,7 @@
                       : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
                   ]"
                 >
-                  {{ dimensionName }}
+                  {{ t(dimensionName) }}
                 </button>
               </nav>
             </div>
