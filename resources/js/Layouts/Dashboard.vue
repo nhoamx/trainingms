d64b816e3cb55841eb8e48db97429e198f0f7dc1
Alfredo Galvan <chencho.backup@gmail.com>
2025-12-14 23:14:46 -0600

    chore: remove notification bell

--- resources/js/Layouts/Dashboard.vue
+++ resources/js/Layouts/Dashboard.vue
@@ -77,11 +77,11 @@
                     </div>
                     <div class="hidden md:block">
                         <div class="ml-4 flex items-center md:ml-6">
-                            <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                            <!-- <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                                 <span class="absolute -inset-1.5" />
                                 <span class="sr-only">View notifications</span>
                                 <BellIcon class="size-6" aria-hidden="true" />
-                            </button>
+                            </button> -->
 
                             <!-- Profile dropdown -->
                             <Menu as="div" class="relative ml-3">
@@ -135,11 +135,11 @@
                             <div class="text-base/5 font-medium text-white">{{ user.name }}</div>
                             <div class="text-sm font-medium text-gray-400">{{ user.email }}</div>
                         </div>
-                        <button type="button" class="relative ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                        <!-- <button type="button" class="relative ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                             <span class="absolute -inset-1.5" />
                             <span class="sr-only">View notifications</span>
                             <BellIcon class="size-6" aria-hidden="true" />
-                        </button>
+                        </button> -->
                     </div>
                     <div class="mt-3 space-y-1 px-2">
                         <DisclosureButton v-for="item in userNavigation" :key="item.name" as="a" :href="item.href" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">{{ item.name }}</DisclosureButton>
