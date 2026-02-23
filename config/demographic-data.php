78f27c42d59a04755ce3397633741e310aa2280c
Alfredo Galvan <101598550+alfredogalvan@users.noreply.github.com>
2026-02-23 07:40:22 -0600

    Update demographic-data.php

--- config/demographic-data.php
+++ config/demographic-data.php
@@ -4,10 +4,8 @@ return [
     'work_schedule' => [
         'active' => true,
         'options' => [
-            'morning' => 'Matutino',
-            'afternoon' => 'Vespertino',
-            'night' => 'Nocturno',
-            'office' => 'Oficina',
+            '6_20' => '6:00 a 20:00',
+            '20_6' => '20:00 a 6:00',
         ],
     ],
 ];
